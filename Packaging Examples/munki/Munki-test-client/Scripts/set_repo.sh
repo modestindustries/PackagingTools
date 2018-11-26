ffee8cf45e668d3e04c6d2b598db6fdf0cfc4397
Modest Industries <info@modestindustries.com>
2018-11-26 12:19:19 -0800

    Updated .gitignore

--- Packaging Examples/munki/Munki-test-client/Scripts/set_repo.sh
+++
@@ -1,2 +0,0 @@
-sudo defaults write /Library/Preferences/ManagedInstalls SoftwareRepoURL "http://newmunki.example.lan/munki_repo"
-sudo defaults write /Library/Preferences/ManagedInstalls ClientIdentifier "test-client"
